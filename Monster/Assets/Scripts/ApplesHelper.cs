8ea2db4b207f87a2da1e32873d77203a876a8996
ZXC <zxc101@mail.ru>
2017-10-25 22:07:05 +0300

    Created project. Created animation of monsters movement. Created scripts for monster, spawner apples, good apple and bad apple.

---
+++ Monster/Assets/Scripts/ApplesHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApplesHelper : MonoBehaviour {
+
+    public int speed;
+    public GameObject pers;
+    public bool isDamaging;
+    public int healthPoints;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("DestroyObject", 3, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == pers.name)
+        {
+            col.SendMessage((isDamaging) ? "TakeDamage" : "Healing", healthPoints);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void DestroyObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Monster/Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject[] obj;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnObject", 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void spawnObject()
+    {
+        int index = (int)Random.Range(0.0f, obj.Length);
+        Instantiate(obj[index], transform.position, transform.rotation);
+    }
+}
